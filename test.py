3575808298fd1452c03403dc6136de79dee47a4b
Pia <gayeongparkk@gmail.com>
2024-05-15 16:09:40 +0900

    i got test

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import os
+from pathlib import Path
 from hdp import run_cairo
 
-def test_run_cairo(capsys):
-    input_file = 'tests/test_file/input.json'
-    output_file = 'tests/test_file/output.zip'
+def test_run_cairo():
+    input_file = 'test_fixture/input.json'
+    output_file = 'test_fixture/output.zip'
     
     output = run_cairo(input_file, output_file)
-    
-  
+    print(output)
+
+test_run_cairo()
